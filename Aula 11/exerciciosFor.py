f64c6d64c176a06dab86aa17fe9b059c35302a50
LayLeo <layusais23+tds23@gmail.com>
2026-04-24 21:24:51 -0300

    aula 11 repeticoes for

---
+++ Aula 11/exerciciosFor.py
@@ -0,0 +1,32 @@
+#Imprima todos os numeros de 0 ate 1000
+
+#numeros = 0
+
+# for i in range(1001):
+ #   print(i)
+
+
+# imprima todos os numeros pares de 0 ate 1000
+
+#for i in range(0, 1001, 2):
+ #   print(i)
+#    start stop step
+#
+
+
+#for i in range(1,1001): #coloque 1 no comeco (start) pra ser diferente de zero
+##        print(i)
+ 
+# Iprima a soma dos numeros pares de 0 ate 10000
+soma = 0
+contador = 0
+ for i in range(0,1001):
+    if i % 2 == 0:
+       soma += i
+       contador += 1
+       produto = 1
+
+
+# Imprima a quantidade de numeros pares de 0 ate 1000
+
+for i in range 
---
+++ Aula 11/repeticoes.py
@@ -0,0 +1,17 @@
+# frutas = ["banana", "maçã", "laranja", "uva", "kiwi", "goiaba"]
+
+# for elemento in lista :
+# for (i = 0)
+
+# for frutas in frutas:
+#    print(f"{frutas} isso é uma fruta")
+
+
+###############################################################
+
+# for i in range(5):
+#      nome = input(f"digite seu nome") 
+#      
+# aça um programa que imprime a soma dos numeros de 0 a 500
+#  numero = 100
+#  numero += 50 
