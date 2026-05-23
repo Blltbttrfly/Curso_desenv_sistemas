fd5bae8064cd3ca9a99b141e2edd4f08dac6f868
LayLeo <layusais23+tds23@gmail.com>
2026-05-22 21:22:50 -0300

    aula 28

---
+++ Aula 28/Olimpiada.py
@@ -0,0 +1,12 @@
+# Critério 1 - Qualidade do Enunciado
+
+# Clareza, narrativa envolvente, exemplos bem construídos e coerência com a
+# solução.
+
+# Critério 2 - Criatividade do Nome da Equipe
+
+# Originalidade, relação com tecnologia/programação e impacto.
+
+# Critério 3 - Participação dos Membros
+
+# Envolvimento e contribuição de todos os 4 integrantes durante a Fase 1.
---
+++ Aula 28/cadastroFuncionario.py
@@ -0,0 +1,11 @@
+while True:
+    print(f"""
+BEM VINDO AO SISTEMA RH XYZ
+Menu: 
+          1. Cadastrar Funcionário
+          2. 
+          3.
+          4.          
+
+""")
+    
